a02e86e6e01c497ccc2ae2df6baebe163b993ace
Michael Morehead <mmorehea@mix.wvu.edu>
2020-11-25 13:13:52 -0500

    shaping up nicely

--- syAutoProject.py
+++ syAutoProject.py
@@ -3,6 +3,7 @@ import os
 import glob
 import tkinter as tk
 from tkinter import filedialog 
+import tkinter.scrolledtext as tkscrolled
 import syglass
 from syglass import pyglass
 import code
@@ -33,7 +34,7 @@ class Application(tk.Frame):
 		self.output["command"] = self.outputBrowseFiles
 		self.output.pack(side="top") 
 		
-		self.textbox = tk.Text(self, height=10, width=70)
+		self.textbox = tkscrolled.ScrolledText(self, height=10, width=70)
 		self.textbox.pack(side="top")
 		self.textbox['state'] = 'disabled'
 		self.textbox['foreground'] = 'white'
@@ -51,15 +52,13 @@ class Application(tk.Frame):
 	def inputBrowseFiles(self): 
 		self.inputFoldername = filedialog.askdirectory(initialdir = "/", title = "Select a Folder") 
 		# Change label contents 
-		self.input["text"] = "Input Folder: " + self.inputFoldername		
+		self.input["text"] = "Input Folder: " + self.inputFoldername
 		
 	def addTextLine(self, text):
-		self.screenMessages.append(text)
-		if len(self.screenMessages) > 6:
-			self.screenMessages.pop()
 		self.textbox['state'] = 'normal'
-		self.textbox.insert('1.0',"\n ".join(self.screenMessages))
+		self.textbox.insert(tk.END, "\n"+text)
 		self.textbox['state'] = 'disabled'
+		self.textbox.see("end")
 		self.update()
 
 	def convert(self):
@@ -74,7 +73,7 @@ class Application(tk.Frame):
 				l = glob.glob(os.path.join(fullpath, "*.png"))
 				if len(l) > 4:
 					dirs.append(fullpath)
-		
+					
 		if not os.path.exists(outputFoldername):
 			os.makedirs(outputFoldername)
 			
@@ -84,6 +83,9 @@ class Application(tk.Frame):
 			self.addTextLine(msg)
 			newPath = os.path.join(outputFoldername, basename)
 			self.addTextLine("Creating...")
+			if os.path.exists(newPath):
+				self.addTextLine("Project found already in Output Folder, skipping...")
+				continue
 			try:
 				project = pyglass.CreateProject(pyglass.path(outputFoldername), basename)
 				dd = pyglass.DirectoryDescription()
@@ -92,23 +94,27 @@ class Application(tk.Frame):
 				dd.InspectByReferenceFile(firstPNG)
 				dataProvider = pyglass.OpenPNGs(dd.GetFileList())
 				cd = pyglass.ConversionDriver()
+				
 				cd.SetInput(dataProvider)
 				cd.SetOutput(project)
 				cd.StartAsynchronous()
+				lowPercentage = 0
+				print(cd.GetPercentage())
+				while cd.GetPercentage() < 100:
+					if cd.GetPercentage() > (lowPercentage + 10):
+						lowPercentage = cd.GetPercentage()
+						self.addTextLine("Progress: " + str(cd.GetPercentage())[0:4] + "%")
 			except Exception as e:
 				self.addTextLine("Something went wrong...")
 				self.addTextLine(str(e))
 			else:
 				self.addTextLine("Success...!")
-			
-		
-		print(dirs)
+		#afterward, use pyglass::VolumeLibrary to reload, Call VolumeLibrary::ReloadLibrary(), Call VolumeLibrary::PutEntry()
 
 def main():
 	root = tk.Tk()
 	ex = Application(root)
 	root.mainloop()
-
 
 if __name__ == '__main__':
 	main()
